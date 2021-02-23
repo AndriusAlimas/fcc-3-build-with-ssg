b9f03cf0d809df10eb92faa421958d92071cf77b
Andrius <alimasWebDeveloper@gmail.com>
2021-02-23 17:28:20 +0000

    descriptive env for API KEY

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // Want to use async/await? Add the `async` keyword to your outer function/method.
 module.exports = async function () {
     try {
-      const response = await axios.get(`http://newsapi.org/v2/everything?domains=bbc.co.uk&apiKey=${process.env.API_KEY}&pageSize=5`);
+      const response = await axios.get(`http://newsapi.org/v2/everything?domains=bbc.co.uk&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
       return response.data;
     } catch (error) {
       console.error(error);
